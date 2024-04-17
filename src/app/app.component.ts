8de0509d175d7418c2a7045bce5cf1471bebf8cf
Pikulkaew Boonpeng <pkboonpeng@gmail.com>
2024-04-17 18:46:02 -0400

    add a top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { topBar } from './shared/topBar/topBar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, topBar],
 })
 export class AppComponent {
   title = 'ng-flix';
---
+++ src/app/shared/topBar/topBar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './topBar.component.html',
+  styleUrl: './topBar.component.scss',
+})
+export class topBar {}
