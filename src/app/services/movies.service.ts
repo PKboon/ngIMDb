882ea234d38cccf73cfc06a96dfb516378da4dda
Pikulkaew Boonpeng <pkboonpeng@gmail.com>
2024-05-04 16:29:58 -0400

    add pipe to the movie service

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MovieDto } from '../types/movie';
 import { apiKey, serviceBaseUrl } from '../contants';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,8 @@ export class MoviesService {
   constructor(private httpClient: HttpClient) {}
 
   getMoviesByType(type: string) {
-    return this.httpClient.get<MovieDto>(
-      `${serviceBaseUrl}/movie/${type}?api_key=${apiKey}`
-    );
+    return this.httpClient
+      .get<MovieDto>(`${serviceBaseUrl}/movie/${type}?api_key=${apiKey}`)
+      .pipe(map((data) => data.results));
   }
 }
