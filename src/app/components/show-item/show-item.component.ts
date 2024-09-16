0814562da6f2ee842c793e2aaf559d6c236d36b6
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2024-09-16 13:22:01 -0400

    add details dynamic routing

--- src/app/components/show-item/show-item.component.ts
+++ src/app/components/show-item/show-item.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Movie } from '../../types/movie';
 import { CommonModule } from '@angular/common';
 import { imageBaseUrl } from '../../contants';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'show-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './show-item.component.html',
   styleUrl: './show-item.component.scss',
 })
